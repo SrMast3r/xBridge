c6ec90d88040055e12ce331dd5bafc2097b26159
SrMast3r <43978734+SrMast3r@users.noreply.github.com>
2022-07-07 01:58:34 -0500

    Update JoinProcessor.java

--- spigot/src/main/java/me/tofpu/speedbridge/game/process/processors/JoinProcessor.java
+++ spigot/src/main/java/me/tofpu/speedbridge/game/process/processors/JoinProcessor.java
@@ -12,9 +12,9 @@ import org.bukkit.entity.Player;
 public class JoinProcessor extends GameProcessor {
     @Override
     public void process(final GameServiceImpl gameService,
-            final Island island, final User user,
-            final Player player,
-            final ProcessType type) {
+                        final Island island, final User user,
+                        final Player player,
+                        final ProcessType type) {
 
         switch (type) {
             case PROCESS:
@@ -25,58 +25,34 @@ public class JoinProcessor extends GameProcessor {
                 // setting the island takenBy to this user
                 // for availability reasons
                 island.takenBy(user);
+                //  player.sendMessage("§e§lBRIDGE §8» §fListo para jugar %player% ");
 
-                player.sendMessage("§e§lBRIDGE §8» §fPrueba Game Runnable");
+                player.performCommand("say " + player.getName());
 
-                // teleporting player to the island's location
-                // MULTI-WORLD PATCH
                 player.teleport(island.location());
-
-                // removing their effects
                 player.getActivePotionEffects().clear();
-
-                // setting their gamemode to survival
                 player.setGameMode(GameMode.SURVIVAL);
-                // resetting their health back to the max health attribute
                 player.setHealth(player.getMaxHealth());
-                // resetting their food levels back to full
                 player.setFoodLevel(20);
-
-                // storing an instance of user & the island that
-                // they're in for the runnable to keep track of them
                 gameService.gameChecker().put(user, island);
-
-                // if the runnable is paused
                 if (gameService.runnable().isPaused()) {
-                    // since the runnable is paused
-                    // we'll start it
                     gameService.runnable().resume();
                 }
                 break;
             case REVERSE:
-                // resetting the island for the next player
                 gameService.resetIsland(user.properties().islandSlot());
-                // resetting the user's properties slot to null
-                // since they're not playing anymore
                 user.properties().islandSlot(null);
-
-                // removing them from our tracker
                 gameService.gameTimer().remove(player.getUniqueId());
                 gameService.gameChecker().remove(user);
 
-                // if the gameChecker is empty
                 if (gameService.gameChecker().isEmpty()) {
-                    // since no one is playing right now
-                    // we'll stop the runnable to save up resources
                     gameService.runnable().pause();
                 }
 
-                // teleporting the player to the lobby location
                 player.teleport(gameService.lobbyService().getLobbyLocation());
                 break;
         }
         // processing the items to this user
         Process.ITEM_JOIN.process(user, player, type);
-        player.sendMessage("§e§lBRIDGE §8» §fPrueba Game Runnable");
     }
 }
